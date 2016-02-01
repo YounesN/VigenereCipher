6e4b0bc8e5af8359ac9b4d84b3945cdb5c7e26f2
YounesN <younes.nej2008@gmail.com>
2016-01-31 22:16:33 -0500

    Initial commit including source file

---
+++ src/encrypt.py
@@ -0,0 +1,5 @@
+info = "This is the Vigenere cipher encryption implementation.";
+info += "\nYou need to put your key in \"key.txt\"";
+info += "\nand your plain text in \"input.txt\".";
+info += "\nAfter you run the program your cipher text will be \"output.txt\".";
+print info;
